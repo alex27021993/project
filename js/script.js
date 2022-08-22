4ef90e7dcae26d9dbb7179c0d13ae4c411dd4f59
Alex27021993 <alex27021993@gmail.com>
2022-08-22 12:25:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+a = 15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 20;
+
+console.log(obj);
+
+var name1 = 'Ívan';
+
+var name = 'Álex';
+
+console.log(name1); {
+    var result = 50;
+}
+
+console.log(result);
